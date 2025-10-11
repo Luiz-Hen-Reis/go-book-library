80bf8b9e92b3560224fa4b49ebdaa0aadce1f254
Luiz Henrique <lhcontato2020@gmail.com>
2025-10-11 18:09:14 -0300

    chore: add create authors table migration

---
+++ internal/store/pgstore/migrations/001_create_authors_table.sql
@@ -0,0 +1,14 @@
+-- Write your migrate up statements here
+
+CREATE TABLE IF NOT EXISTS authors (
+  id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  name VARCHAR(50) NOT NULL,
+  bio TEXT
+);
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS authors;
+
+-- Write your migrate down statements here. If this migration is irreversible
+-- Then delete the separator line above.
