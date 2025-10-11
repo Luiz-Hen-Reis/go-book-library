3eb9a68df97d5c686afab631aeab5fb400c5592e
Luiz Henrique <lhcontato2020@gmail.com>
2025-10-11 18:38:16 -0300

    feat: generate methods for authors operations using sqlc

---
+++ internal/store/pgstore/queries/authors.sql
@@ -0,0 +1,15 @@
+-- name: CreateAuthor :one
+INSERT INTO authors (name, bio)
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: GetAuthorByID :one
+SELECT * FROM authors
+WHERE id = $1;
+
+-- name: ListAuthors :many
+SELECT * FROM authors
+ORDER BY name ASC;
+
+-- name: DeleteAuthor :exec
+DELETE FROM authors WHERE id = $1;
